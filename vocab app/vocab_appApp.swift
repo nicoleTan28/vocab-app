4a871d8290c671c8ed60c7d59c9008b324dfaadf
Nicole Tan <geniusnic009@gmail.com>
2023-08-20 18:50:50 +0800

    Initial Commit

---
+++ vocab app/vocab_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  vocab_appApp.swift
+//  vocab app
+//
+//  Created by T Krobot on 20/8/23.
+//
+
+import SwiftUI
+
+@main
+struct vocab_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
